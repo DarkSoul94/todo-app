057e0193589ce9e4e9fb3d3aff99753cf74576cf
DarkSoul94 <tischenkoslava92@gmail.com>
2022-04-12 13:58:35 +0300

    component NewTask

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.scss';
+import NewTask from './components/NewTask/NewTask';
 import TaskItem from './components/Task/TaskItem';
-
-type Task = {
-  id: number
-  text: string
-  category: string
-  checked: boolean
-}
+import { Task } from './types';
 
 const Tasks: Array<Task> = [
   { id: 1, text: "Get a new helmet", category: "Uncategorized", checked: true },
@@ -16,6 +11,7 @@ const Tasks: Array<Task> = [
 
 function App() {
   const [tasks, setTasks] = useState<Array<Task>>([])
+  const [category, setCategory] = useState<string>("Uncategorized")
 
   useEffect(() => {
     setTasks(Tasks)
@@ -25,7 +21,7 @@ function App() {
     setTasks(prev => {
       prev.forEach(task => {
         if (task.id === taskID) {
-          task.checked = !task.checked
+          task.checked = checkState
         }
       });
       return prev
@@ -38,24 +34,27 @@ function App() {
     <TaskItem key={task.id} id={task.id} text={task.text} category={task.category} checked={task.checked} onCheck={check} onDelete={deleteTask} />
   ))
 
+  const crateTask = (newTask: Task) => {
+    newTask.id = tasks.length + 1
+    setTasks(prev => [...prev, newTask])
+  }
+
   return (
     <div className="wrapper">
       <div className="category-list">
         <div className="category-item">
-          All
+          <span>All</span>
         </div>
         <div className="category-item">
-          Groceries
+          <span>Groceries</span>
         </div>
         <div className="category-item">
-          College
+          <span>College</span>
         </div>
       </div>
       <div className="content">
         <h1>All Tasks</h1>
-        <div className="new-task">
-          <input type="text" placeholder='Add a new task insdie ‘All’ category' />
-        </div>
+        <NewTask taskCategory={category} createTask={crateTask} />
         <div className="task-list">
           {taskList}
         </div>
---
+++ frontend/src/components/NewTask/NewTask.tsx
@@ -0,0 +1,45 @@
+import { ChangeEvent, FC, KeyboardEvent, useState } from "react";
+import { Task } from "../../types";
+import "./NewTask.scss"
+
+interface Props {
+  taskCategory: string
+  createTask(newTask: Task): void
+}
+
+const NewTask: FC<Props> = ({ taskCategory, createTask }) => {
+  const [taskText, setTaskText] = useState<string>("")
+
+  const clearInput = () => {
+    setTaskText("")
+  }
+  
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTaskText(e.target.value)
+  }
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      let newTask: Task = {
+        id: 0,
+        text: taskText,
+        category: taskCategory,
+        checked: false
+      }
+      clearInput()
+      createTask(newTask)
+    }
+
+    if (e.key === "Escape") {
+      clearInput()
+    }
+  }
+
+  return (
+    <div className="new-task">
+      <input type="text" placeholder={"Add a new task insdie ‘" + taskCategory + "’ category"} value={taskText} onChange={onChange} onKeyDown={onKeyDown} />
+    </div>
+  )
+}
+
+export default NewTask
